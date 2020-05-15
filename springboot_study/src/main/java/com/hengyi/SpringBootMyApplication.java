76c8a550c0d9a74b92944048ebaf327db86f6495
chen <2501238913@qq.com>
2020-05-15 08:54:26 +0800

    提交测试代码3

---
+++ springboot_study/src/main/java/com/hengyi/SpringBootMyApplication.java
@@ -0,0 +1,11 @@
+package com.hengyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMyApplication.class) ;
+    }
+}
